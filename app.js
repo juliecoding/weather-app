115df5945a05b6695b23c64853a6b6b055243ff6
juliecoding <julie.akohler@gmail.com>
2017-05-03 00:23:46 -0600

    Install yargs

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to get weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
